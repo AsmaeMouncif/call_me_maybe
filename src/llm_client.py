3f55c492c6049ffaa863a9a2d27a8054453e94c6
ASMAE MOUNCIF <158608762+AsmaeMouncif@users.noreply.github.com>
2026-08-26 18:53:15 +0000

    ok

---
+++ src/llm_client.py
@@ -0,0 +1,16 @@
+import json
+from llm_sdk import Small_LLM_Model
+
+
+def load_model() -> Small_LLM_Model:
+    return Small_LLM_Model()
+
+
+def load_vocab(model: Small_LLM_Model) -> dict[str, int]:
+    path = model.get_path_to_vocab_file()
+    with open(path, "r", encoding="utf-8") as f:
+        vocab: dict[str, int] = json.load(f)
+    return vocab
+
+def build_reverse_vocab(vocab: dict[str, int]) -> dict[int, str]:
+    return {v: k for k, v in vocab.items()}
