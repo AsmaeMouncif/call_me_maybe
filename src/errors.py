75adfe574238b3fa77973dcf90f96f777a6a10a6
asmounci <asmaemouncif4@gmail.com>
2026-08-31 08:31:38 +0100

    ok

--- src/errors.py
+++ src/errors.py
@@ -1,13 +1,18 @@
 class CallMeMaybeError(Exception):
+    pass
 
 
 class InputError(CallMeMaybeError):
+    pass
 
 
 class VocabError(CallMeMaybeError):
+    pass
 
 
 class GenerationError(CallMeMaybeError):
+    pass
 
 
 class OutputError(CallMeMaybeError):
+    pass
