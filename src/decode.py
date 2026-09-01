3ec21b8550fe3fb31bb5b2e01701b1c037e76ec7
asmounci <asmaemouncif4@gmail.com>
2026-09-01 21:33:43 +0100

    ok

--- src/decode.py
+++ src/decode.py
@@ -1,7 +1,8 @@
 from collections.abc import Iterable
+import numpy as np
 
 
 def pick_from_ids(logits: list[float], allowed: Iterable[int]) -> int:
 
 
-candidates = np.fromiter(allowed,dtype=int)
+candidates = np.fromiter(allowed,dtype=int64)
