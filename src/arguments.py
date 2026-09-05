9b1545f3ae657f1cd1ce3f685b0c810d59b11f85
asmounci <asmaemouncif4@gmail.com>
2026-09-05 02:40:09 +0100

    ok

---
+++ src/arguments.py
@@ -0,0 +1,15 @@
+
+
+def get_number_start_tokens() -> set[int]:
+    return set(range(15, 25)) | {12}
+
+
+def get_number_digit_tokens() -> set[int]:
+    return set(range(15, 25))
+
+
+def get_number_end_tokens(is_last_parameter: bool) -> set[int]:
+    if is_last_parameter:
+        return {92}
+    return {11}
+
